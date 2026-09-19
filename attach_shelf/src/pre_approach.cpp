8b7370f9fd5970867df1adf6d31a5bd97a2168bc
Micko8 <mickael@syscontrol.tech>
2026-09-19 16:36:30 +0000

    PreApproach Node Init

---
+++ attach_shelf/src/pre_approach.cpp
@@ -0,0 +1,20 @@
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include <algorithm>
+#include <chrono>
+#include <sensor_msgs/msg/laser_scan.hpp>
+
+class PreApproach : public rclcpp::Node {
+public:
+  PreApproach() : Node("move_robot_node") {}
+
+private:
+};
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PreApproach>());
+  rclcpp::shutdown();
+  return 0;
+}
