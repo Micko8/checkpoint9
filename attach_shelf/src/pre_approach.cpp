5d7d40be0269acbca4fe29ae63007f18e8ccee97
Micko8 <mickael@syscontrol.tech>
2026-09-19 17:15:21 +0000

    PreApproach sub+pub init

--- attach_shelf/src/pre_approach.cpp
+++ attach_shelf/src/pre_approach.cpp
@@ -1,6 +1,7 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "rmw/types.h"
 #include <algorithm>
 #include <chrono>
 #include <sensor_msgs/msg/laser_scan.hpp>
@@ -10,9 +11,29 @@ public:
   PreApproach() : Node("preapproach_node") {
 
     RCLCPP_INFO(this->get_logger(), "Preapproach : Constructor");
+
+    rclcpp::QoS qos_profile(10);
+    qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+    qos_profile.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel",
+                                                                   qos_profile);
+
+    subscriber_laser_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "/scan", qos_profile,
+        std::bind(&PreApproach::laser_callback, this, std::placeholders::_1));
   }
 
 private:
+  void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+    /*
+    laser_scan_ = *msg;
+    laser_data_received_ = true;
+*/
+  }
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr
+      subscriber_laser_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 };
 
 int main(int argc, char *argv[]) {
