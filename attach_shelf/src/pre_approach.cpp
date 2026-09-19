f5567886c4fdea218e7ada3e1afa7222fda101e5
Micko8 <mickael@syscontrol.tech>
2026-09-19 17:26:12 +0000

    PreApproach param as arguments init

--- attach_shelf/src/pre_approach.cpp
+++ attach_shelf/src/pre_approach.cpp
@@ -12,6 +12,17 @@ public:
 
     RCLCPP_INFO(this->get_logger(), "Preapproach : Constructor");
 
+    // Declare parameters with default values
+    this->declare_parameter<double>("obstacle", 0.0);
+    this->declare_parameter<int>("degrees", 0);
+
+    // Read parameter values
+    obstacle_ = this->get_parameter("obstacle").as_double();
+    degrees_ = this->get_parameter("degrees").as_int();
+
+    RCLCPP_INFO(this->get_logger(), "obstacle: %.2f", obstacle_);
+    RCLCPP_INFO(this->get_logger(), "degrees: %d", degrees_);
+
     rclcpp::QoS qos_profile(10);
     qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
     qos_profile.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
@@ -31,6 +42,8 @@ private:
     laser_data_received_ = true;
 */
   }
+  float obstacle_;
+  int degrees_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr
       subscriber_laser_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
